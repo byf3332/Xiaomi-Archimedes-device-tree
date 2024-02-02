75395e83a3fbfeaef11f5361b11f225dc9ee292e
byf3332 <71202267+byf3332@users.noreply.github.com>
2024-02-02 22:30:30 +0800

    暂时移除/vendor相关内容

--- omni_Archimedes.mk
+++ omni_Archimedes.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from Archimedes device
 $(call inherit-product, device/xiaomi/Archimedes/device.mk)
